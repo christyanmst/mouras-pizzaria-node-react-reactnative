e5dde69c03723b05b690068430087a96b8782d10
christyanmst <christyan228@gmail.com>
2024-02-17 01:57:34 -0300

    feat: :sparkles: UserService, UserController
    Criando Usuário, configurando service e controller

---
+++ mouras-pizzaria-api/src/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { UserService } from "../services/UserService";
+
+class UserController {
+    async createUser(req: Request, res: Response) {
+        console.log(req.body);
+        const { name, email, password } = req.body;
+
+        const userService = new UserService();
+
+        const user = await userService.createUser({ name, email, password });
+
+        return res.json(user)
+    }
+}
+
+export { UserController }
---
+++ mouras-pizzaria-api/src/services/UserService.ts
@@ -0,0 +1,42 @@
+import prismaClient from "../prisma";
+import { hash } from "bcryptjs";
+
+interface CreateUserRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+class UserService {
+    async createUser({ name, email, password }: CreateUserRequest) {
+
+        if(!(name && email && password)) throw new Error("Missing parameters");
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (userAlreadyExists) throw new Error("User already exists");
+
+        const passwordHash = await hash(password, 8);
+
+        const user = await prismaClient.user.create({
+            data: {
+                username: name,
+                email: email,
+                password: passwordHash
+            },
+            select: {
+                id: true,
+                username: true,
+                email: true,
+            }
+        })
+
+        return user
+    }
+}
+
+export { UserService }
